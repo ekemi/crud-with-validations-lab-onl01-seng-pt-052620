c90023c407210c3ee437cd0f1c8d60fa0bf014e3
edmundo papi <edmundopierre@yahoo.com>
2020-11-27 10:16:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,9 +38,7 @@ def create
    @song = Song.find(params[:id])
  end
  def song_params
-prams.require(:song).permit(
-:title,:released,:release_year,:artist_name,:genre)
- end
+prams.require(:song).permit(:title,:released,:release_year,:artist_name,:genre)
 end
 
 end
